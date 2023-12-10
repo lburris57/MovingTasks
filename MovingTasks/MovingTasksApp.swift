0a9150e451a270e4e4205b270ec4c7af37368b8a
lburris57 <lburris@mac.com>
2023-12-10 15:40:19 -0500

    Initial Commit

---
+++ MovingTasks/MovingTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovingTasksApp.swift
+//  MovingTasks
+//
+//  Created by Larry Burris on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MovingTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
